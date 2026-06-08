d9c389a194aae0f2ebb2ab483abc612657b48b59
findgecko <findgecko@users.noreply.github.com>
2026-06-08 13:08:24 +0800

    Set AC-2 status to sold

--- data/geckos.js
+++ data/geckos.js
@@ -91,7 +91,7 @@ window.GECKOS_DATA = {
       "birth": "2026/3/4",
       "weight": "",
       "price": "",
-      "status": "reserved",
+      "status": "sold",
       "note": "照片拍攝於 2026.6.4",
       "photos": [
         "IMG_5938.jpg",
